7b42a187d6371f635fc9296ee4d65f594d0b3074
NumAniCloud <num.ani.cloud@gmail.com>
2015-04-26 21:46:25 +0900

    ファイルパス指定方法を変更。

--- CSharpWrapperGenerator/Program.cs
+++ CSharpWrapperGenerator/Program.cs
@@ -12,23 +12,32 @@ namespace CSharpWrapperGenerator
 	{
 		static void Main(string[] args)
 		{
+			if(args.Length < 1)
+			{
+				Console.WriteLine("第１引数に設定ファイルを指定してください");
+				return;
+			}
+
 			Settings settings;
-			var settingFilePath = "CSharpWrapperGeneratorSettings.json";
+			var settingFilePath = args[0];
 			var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Settings));
 			using (var file = File.Open(settingFilePath, FileMode.Open))
 			{
 				settings = serializer.ReadObject(file) as Settings;
 			}
 
+			var settingsDirectory = Path.GetDirectoryName(args[0]);
+
 			var doxygenParser = new DoxygenParser();
-			doxygenParser.AddNamespaceFile(Path.Combine(settings.DoxygenXmlDirPath, "namespaceace.xml"));
-			var docs = Directory.EnumerateFiles(settings.DoxygenXmlDirPath, "classace_*.xml", SearchOption.TopDirectoryOnly).ToArray();
+			doxygenParser.AddNamespaceFile(Path.Combine(settingsDirectory, settings.DoxygenXmlDirPath, "namespaceace.xml"));
+			var docs = Directory.EnumerateFiles(Path.Combine(settingsDirectory, settings.DoxygenXmlDirPath), "classace_*.xml", SearchOption.TopDirectoryOnly).ToArray();
 			doxygenParser.AddClassFiles(docs);
 
 			var csharpParser = new CSharpParser();
-			var cs = Directory.EnumerateFiles(settings.SwigCSharpDirPath, "*.cs", SearchOption.AllDirectories).ToArray();
+			var cs = Directory.EnumerateFiles(Path.Combine(settingsDirectory, settings.SwigCSharpDirPath), "*.cs", SearchOption.AllDirectories).ToArray();
 			csharpParser.Parse(cs);
 
+			settings.ExportFilePath = Path.Combine(settingsDirectory, settings.ExportFilePath);
 			var exporter = new Exporter(settings, doxygenParser.Result, csharpParser.Result);
 			exporter.Export();
 		}
